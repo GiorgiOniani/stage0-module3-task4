593a22eb17a5dad76e5743a5cf9c12dd8439e919
giorgi.oniani.2 <giorgi.oniani.2@btu.edu.ge>
2023-08-02 16:14:58 +0400

    0

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -4,6 +4,14 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a = 1;
         int b = 5;
-        System.out.println(((9*(a * a) - (5 * b) + 2 + a - 7) *( a + b - (4 * (a * b))) / 2));
+
+        int part1 = 9 * a * a - 5 * b + 2 + a - 7;
+        int part2 = (a + b - 4 * a * b) / 2;
+        int part3 = part1 * part2;
+
+        System.out.println(part3);
+
+        boolean formulaCorrect = Math.abs(part3 - ((9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2))) < 1e-10;
+        System.out.println(formulaCorrect);
     }
 }
